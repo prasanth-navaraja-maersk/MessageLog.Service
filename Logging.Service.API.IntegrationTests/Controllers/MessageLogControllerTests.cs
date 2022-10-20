e451996dc510e91024d265a01258a8cd7caa0f0c
Prasanth Navaraja <prasanth.navaraja@maersk.com>
2022-10-20 16:25:34 +0530

    Test for MessageLogsByMessageType

--- Logging.Service.API.IntegrationTests/Controllers/MessageLogControllerTests.cs
+++ Logging.Service.API.IntegrationTests/Controllers/MessageLogControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Web;
 using FluentAssertions;
 using Logging.Service.Application.Requests;
 using Bogus;
@@ -110,6 +111,62 @@ public class MessageLogControllerTests : IClassFixture<ApiWebApplicationFactory>
     }
 
     [Fact]
+    public void Get_MessageLogsByMessageType_LoadTest()
+    {
+        // Arrange
+        var requests = GetMessageLogRequest(1000);
+        var dataFeed = Feed.CreateRandom("requests", requests);
+
+        var stepInsert = Step.Create("Upsert_Message_Logs", feed: dataFeed, async context =>
+        {
+            var resp = await _fixture.CreateClient()
+                .PostAsJsonAsync("/MessageLogs", context.FeedItem, CancellationToken.None);
+
+            return resp.ToNBomberResponse();
+        });
+
+        var stepGet = Step.Create("Get_Message_Logs", feed: dataFeed, async context =>
+        {
+            var query = HttpUtility.ParseQueryString(string.Empty);
+            query["messageType"] = context.FeedItem.MessageType;
+
+            var builder = new UriBuilder("http://localhost.com/MessageLogs/MessageType");
+            builder.Port = -1;
+            builder.Query = query.ToString();
+            string url = builder.ToString();
+
+            var resp = await _fixture.CreateClient()
+                .GetAsync(url, CancellationToken.None);
+
+            return resp.ToNBomberResponse();
+        });
+
+        var scenario = ScenarioBuilder
+            .CreateScenario("Message_Logs", stepInsert, stepGet)
+            .WithoutWarmUp()
+            .WithLoadSimulations(
+                Simulation.KeepConstant(copies: 10, during: TimeSpan.FromSeconds(10)));
+
+        // Act
+        var stats = NBomberRunner
+            .RegisterScenarios(scenario)
+            .WithReportFormats(ReportFormat.Html)
+            .Run();
+
+        // Assert
+        stats.Should().NotBeNull();
+        var stepStats = stats.ScenarioStats[0].StepStats[0];
+
+        stepStats.Fail.Request.Count.Should().Be(0);
+        //stepStats.Ok.Request.Count.Should().BeGreaterThan(1000);
+        //stepStats.Ok.Request.RPS.Should().BeGreaterThan(100);
+        stepStats.Ok.Latency.Percent75.Should().BeLessOrEqualTo(100);
+        stepStats.Ok.DataTransfer.MinBytes.Should().Be(4);
+        //stepStats.Ok.DataTransfer.AllBytes.Should().BeGreaterOrEqualTo(1000L);
+    }
+
+
+    [Fact]
     public async Task Get_MessageLogs_IntegrationTest()
     {
         // Arrange
@@ -120,6 +177,30 @@ public class MessageLogControllerTests : IClassFixture<ApiWebApplicationFactory>
             .PostAsJsonAsync("/MessageLogs", messageLogRequest, CancellationToken.None);
 
         var result = await _fixture.CreateClient().GetAsync("/MessageLogs", CancellationToken.None);
+
+        // Assert
+        result.Should().BeSuccessful();
+    }
+
+    [Fact]
+    public async Task Get_MessageLogs_ByMessageType_IntegrationTest()
+    {
+        // Arrange
+        var messageLogRequest = GetMessageLogRequest();
+
+        var query = HttpUtility.ParseQueryString(string.Empty);
+        query["messageType"] = messageLogRequest.First().MessageType;
+
+        var builder = new UriBuilder("http://localhost.com/MessageLogs/MessageType");
+        builder.Port = -1;
+        builder.Query = query.ToString();
+        string url = builder.ToString();
+
+        _ = await _fixture.CreateClient()
+            .PostAsJsonAsync("/MessageLogs", messageLogRequest!.First(), CancellationToken.None);
+
+        //Act
+        var result = await _fixture.CreateClient().GetAsync(url, CancellationToken.None);
 
         // Assert
         result.Should().BeSuccessful();
