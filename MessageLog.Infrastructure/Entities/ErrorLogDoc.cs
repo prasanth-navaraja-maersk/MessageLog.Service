be83559a19c2cf7d185a3c23ca1f11cf7c66c0ac
Prasanth Navaraja <prasanth.navaraja@maersk.com>
2022-10-21 18:21:14 +0530

    updated entity with comments

--- MessageLog.Infrastructure/Entities/ErrorLogDoc.cs
+++ MessageLog.Infrastructure/Entities/ErrorLogDoc.cs
@@ -5,10 +5,11 @@ namespace MessageLog.Infrastructure.Entities;
 public class ErrorLogDoc : IEntity<long>, ISystemCreateDate, ISystemModifiedDate
 {
     public long Id { get; set; }
-    public long MessageId { get; set; }
-    public string ExternalIdentifier { get; set; }
+    public long? CorrelationId { get; set; }
+    public string? ExternalIdentifier { get; set; }
     public string ErrorCategory { get; set; }
     public string ErrorMessage { get; set; }
+    public string? BlobUrl { get; set; }
     public DateTime? SystemCreateDate { get; set; }
-    public DateTime? SystemModifiedDate { get; }
+    public DateTime? SystemModifiedDate { get; } //Not required
 }
--- MessageLog.Infrastructure/Entities/MessageLogDoc.cs
+++ MessageLog.Infrastructure/Entities/MessageLogDoc.cs
@@ -5,7 +5,7 @@ namespace MessageLog.Infrastructure.Entities;
 public class MessageLogDoc : IEntity<long>, ISystemCreateDate, ISystemModifiedDate
 {
     public long Id { get; set; }
-    public string MessageId { get; set; }
+    public string MessageId { get; set; } //CorelationId
     public string ExternalIdentifier { get; set; }
     public string MessageType { get; set; }
     public string Source { get; set; }
@@ -14,6 +14,9 @@ public class MessageLogDoc : IEntity<long>, ISystemCreateDate, ISystemModifiedDa
     public string Stage { get; set; }
     public bool IsError { get; set; }
     public int Retries { get; set; } = 0;
+    public string? BlobUrl { get; set; }
     public DateTime? SystemCreateDate { get; set; }
     public DateTime? SystemModifiedDate { get; set; }
+
+    //Metadata => store composite key value
 }
